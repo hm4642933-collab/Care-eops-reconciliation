1127b17ec7e15af3a18a119b020ee6038ceb8553
hm4642933-collab <hm4642933@gmail.com>
2026-08-11 15:10:03 +0500

    Refactor property data and total beds logic
    Updated property data and fixed total beds calculation.

--- app.py
+++ app.py
@@ -13,141 +13,147 @@ st.set_page_config(
 PROPERTY_DATA_RAW = """PR ID\tADDRESS\tTOTAL BEDS
 PF0036\tPF0036 - 57 Belgrave Road\t1
 PF0040\tPF0040 - 61 Holtspur Avenue\t1
-PF0042\tPF0042 - Heneage Court, Grange Road, Bucks, SL9 9FB\t1
+PF0042\tPF0042 - Heneage Court, Grange Road, Bucks, SL9 9FB \t1
 PR0003\tPR0003 - NETHERWOODS\t11
 PR0004\tPR0004  137 Stoke Poges Lane, Slough, Berkshire, SL1 3LX\t8
 PR0008\tPR0008 - 246 Stoke Poges Lane\t4
 PR0009\tPR0009 - 12 ELLIS AVENUE\t8
-PR0010\tPR0010 - 8A Regent Court\t1
+PR0010\tPR0010 - 8A Regent Court \t1
 PR0012\tPR0012 - 31 LABURNHAM RD\t10
 PR0015\tPR0015 - 13 RAMSBURY CLOSE\t6
-PR0017\tPR0017 - 100 West Burnham Way\t5
-PR0019\tPR0019 - 19 Lansdowne Avenue\t5
-PR0020\tPR0020 - 75 Lascelles Road\t5
-PR0021\tPR0021 - 85 Lascelles Road\t5
-PR0023\tPR0023 - 33 Mildmay Road\t5
-PR0024\tPR0024 - 44 Mildmay Road\t6
-PR0026\tPR0026 - 69 Mayflower Way\t4
-PR0028\tPR0028 - 22 Broad Keystone\t4
-PR0029\tPR0029 - 8 Broad Keystone\t4
-PR0030\tPR0030 - 32 Broad Keystone\t4
-PR0031\tPR0031 - 25 Broad Keystone\t4
-PR0032\tPR0032 - 13 Broad Keystone\t4
-PR0033\tPR0033 - 10 Broad Keystone\t4
-PR0035\tPR0035 - 34 Broad Keystone\t4
-PR0036\tPR0036 - 29 Broad Keystone\t4
-PR0037\tPR0037 - 24 Broad Keystone\t4
-PR0038\tPR0038 - 14 Broad Keystone\t4
-PR0039\tPR0039 - 26 Broad Keystone\t4
-PR0041\tPR0041 - 23 Broad Keystone\t4
-PR0042\tPR0042 - 12 Broad Keystone\t4
-PR0043\tPR0043 - 15 Broad Keystone\t4
-PR0044\tPR0044 - 30 Broad Keystone\t4
-PR0045\tPR0045 - 28 Broad Keystone\t4
-PR0046\tPR0046 - 27 Broad Keystone\t4
-PR0047\tPR0047 - 31 Broad Keystone\t4
-PR0048\tPR0048 - 9 Broad Keystone\t4
-PR0049\tPR0049 - 18 Broad Keystone\t4
-PR0050\tPR0050 - 11 Broad Keystone\t4
-PR0051\tPR0051 - 21 Broad Keystone\t4
-PR0052\tPR0052 - 17 Broad Keystone\t4
-PR0053\tPR0053 - 19 Broad Keystone\t4
-PR0054\tPR0054 - 33 Broad Keystone\t4
-PR0055\tPR0055 - 20 Broad Keystone\t4
-PR0056\tPR0056 - 2 Broad Keystone\t4
-PR0057\tPR0057 - 4 Broad Keystone\t4
-PR0058\tPR0058 - 6 Broad Keystone\t4
-PR0059\tPR0059 - 7 Broad Keystone\t4
-PR0060\tPR0060 - 5 Broad Keystone\t4
-PR0061\tPR0061 - 3 Broad Keystone\t4
-PR0062\tPR0062 - 1 Broad Keystone\t4
-PR0063\tPR0063 - 16 Broad Keystone\t4
-PR0064\tPR0064 - 38 Broad Keystone\t4
-PR0065\tPR0065 - 40 Broad Keystone\t4
-PR0066\tPR0066 - 37 Broad Keystone\t4
-PR0067\tPR0067 - 35 Broad Keystone\t4
-PR0068\tPR0068 - 39 Broad Keystone\t4
-PR0069\tPR0069 - 36 Broad Keystone\t4
-PR0070\tPR0070 - 75 Dropmore Road\t4
-PR0071\tPR0071 - 31 Burnham Lane\t5
-PR0072\tPR0072 - 33 Burnham Lane\t5
-PR0073\tPR0073 - 61 Burnham Lane\t5
-PR0074\tPR0074 - 59 Burnham Lane\t5
-PR0075\tPR0075 - 57 Burnham Lane\t5
-PR0076\tPR0076 - 55 Burnham Lane\t5
-PR0077\tPR0077 - 29 Burnham Lane\t5
-PR0078\tPR0078 - 35 Burnham Lane\t5
-PR0079\tPR0079 - 37 Burnham Lane\t5
-PR0080\tPR0080 - 39 Burnham Lane\t5
-PR0081\tPR0081 - 41 Burnham Lane\t5
-PR0082\tPR0082 - 43 Burnham Lane\t5
-PR0083\tPR0083 - 45 Burnham Lane\t5
-PR0084\tPR0084 - 47 Burnham Lane\t5
-PR0085\tPR0085 - 49 Burnham Lane\t5
-PR0086\tPR0086 - 51 Burnham Lane\t5
-PR0087\tPR0087 - 53 Burnham Lane\t5
-PR0088\tPR0088 - 14 Church Street\t5
-PR0089\tPR0089 - 16 Church Street\t5
-PR0090\tPR0090 - 18 Church Street\t5
-PR0091\tPR0091 - 20 Church Street\t5
-PR0092\tPR0092 - 22 Church Street\t5
-PR0093\tPR0093 - 24 Church Street\t5
-PR0094\tPR0094 - 26 Church Street\t5
-PR0095\tPR0095 - 28 Church Street\t5
-PR0096\tPR0096 - 30 Church Street\t5
-PR0097\tPR0097 - 32 Church Street\t5
-PR0098\tPR0098 - 34 Church Street\t5
-PR0099\tPR0099 - 36 Church Street\t5
-PR0100\tPR0100 - 38 Church Street\t5
-PR0101\tPR0101 - 40 Church Street\t5
-PR0102\tPR0102 - 42 Church Street\t5
-PR0103\tPR0103 - 44 Church Street\t5
-PR0104\tPR0104 - 46 Church Street\t5
-PR0105\tPR0105 - 48 Church Street\t5
-PR0106\tPR0106 - 50 Church Street\t5
-PR0107\tPR0107 - 52 Church Street\t5
-PR0108\tPR0108 - 54 Church Street\t5
-PR0109\tPR0109 - 56 Church Street\t5
-PR0110\tPR0110 - 58 Church Street\t5
-PR0111\tPR0111 - 60 Church Street\t5
-PR0112\tPR0112 - 62 Church Street\t5
-PR0113\tPR0113 - 64 Church Street\t5
-PR0114\tPR0114 - 66 Church Street\t5
-PR0115\tPR0115 - 68 Church Street\t5
-PR0116\tPR0116 - 70 Church Street\t5
-PR0117\tPR0117 - 72 Church Street\t5
-PR0118\tPR0118 - 74 Church Street\t5
-PR0119\tPR0119 - 76 Church Street\t5
-PR0120\tPR0120 - 78 Church Street\t5
-PF0037\tPF0037 - 25 Farnham Road\t1
-PF0038\tPF0038 - 37 Farnham Road\t1
-PF0039\tPF0039 - 51 Farnham Road\t1
-PF0041\tPF0041 - 63 Holtspur Avenue\t1
-PR0001\tPR0001 - 44 Stoke Poges Lane\t7
-PR0002\tPR0002 - 131 Stoke Poges Lane\t7
-PR0005\tPR0005 - 139 Stoke Poges Lane\t7
-PR0006\tPR0006 - 141 Stoke Poges Lane\t7
-PR0007\tPR0007 - 143 Stoke Poges Lane\t7
-PR0011\tPR0011 - 256 Stoke Poges Lane\t6
-PR0013\tPR0013 - 109 Stoke Poges Lane\t6
-PR0014\tPR0014 - 111 Stoke Poges Lane\t6
-PR0016\tPR0016 - 113 Stoke Poges Lane\t6
-PR0018\tPR0018 - 145 Stoke Poges Lane\t6
-PR0022\tPR0022 - 35 Mildmay Road\t6
-PR0025\tPR0025 - 46 Mildmay Road\t6
-PR0027\tPR0027 - 48 Mildmay Road\t6
-PF0043\tFlat 1, Low Need, Swan Road, West Drayton, London, UB7 7LA\t1
-PF0044\tFlat 2, Low Need, Swan Road, West Drayton, London, UB7 7LA\t1
-PF0045\tFlat 3, Low Need, Swan Road, West Drayton, London, UB7 7LA\t1
-PF0046\tFlat 4, Low Need, Swan Road, West Drayton, London, UB7 7LA\t1
-PF0047\tFlat 5, Low Need, Swan Road, West Drayton, London, UB7 7LA\t1
-PF0048\tFlat 6, Low Need, Swan Road, West Drayton, London, UB7 7LA\t1
-PF0049\tFlat 7, Low Need, Swan Road, West Drayton, London, UB7 7LA\t1
-PF0050\tFlat 8, Low Need, Swan Road, West Drayton, London, UB7 7LA\t1
-PF0051\tFlat 9, Low Need, Swan Road, West Drayton, London, UB7 7LA\t1
-PF0052\tFlat 10, Low Need, Swan Road, West Drayton, London, UB7 7LA\t1
+PR0016\tPR0016 - 65 HONEY END LANE\t6
+PR0017\tPR0017 - 66 HONEY END LANE\t8
+PR0018\tPR0018 - 8 CHARRINGTON ROAD\t6
+PR0019\tPR0019 - Newlands\t11
+PR0020\tPR0020 - 60 LONDON ROAD\t6
+PR0023\tPR0023 - 99-101 LONDON ROAD\t7
+PR0025\tPR0025 - 64 QUEEN MARY AVENUE\t6
+PR0027\tPR0027 - 15 ASCOTT ROAD\t6
+PR0028\tPR0028  Lanterns, 42 West Road, Guildford, Surrey, GU1 2AT\t9
+PR0029\tPR0029 - 242 Tileshurt Road\t6
+PR0031\tPR0031 - 166 Green Lane\t4
+PR0033\tPR0033 - 81 Norbiton Avenue\t6
+PR0034\tPR0034 - 3 Bodley Road\t10
+PR0035\tPR0035 - 3 Sandhurst Avenue\t6
+PR0038\tPR0038 - 11 Clarendon Court\t1
+PR0041\tPR0041 - 141 Bridge Road\t9
+PR0043\tPR0043 - 11A Regent Court\t1
+PR0045\tPR0045 - Flat 4, 14 Queens Gate\t1
+PR0046\tPR0046 - Hazel Lodge\t13
+PR0048\tPR0048 - 41B Sussex Place\t1
+PR0051\tPR0051 - 22 Cotts Wood Drive, Guildford, GU4 7RB\t1
+PR0052\tPR0052 - Boundary Cottage\t6
+PR0054\tPR0054 - 47 Victoria Road\t6
+PR0055\tPR0055 - 41C Sussex Place\t1
+PR0056\tPR0056 - 182 Cookham Road, Maidenhead, SL6 7HP, Started 28/03/14\t6
+PR0057\tPR0057 - 3 Thamesbridge Court, Ray Park Avenue, Maidenhead, SL6 8DS\t1
+PR0058\tPR0058 - 57 Robinhood Road\t6
+PR0059\tPR0059 - 5 Bicester ROAD\t7
+PR0060\tPR0060 - 233 Stoke Road\t5
+PR0061\tPR0061 - 348 Kingston Road\t4
+PR0062\tPR0062 - 410 Hill Cross Avenue\t7
+PR0063\tPR0063 - Noel Cottage, Maybury Hill, Woking, Surrey, GU22 8AH\t6
+PR0064\tPR0064 - 11 Oakleigh Avenue\t6
+PR0065\tPR0065 - 9 Ledgers Road\t6
+PR0066\tPR0066 - 5 Ascott Court, Aylesbury, HP20 1HQ\t1
+PR0067\tPR0067 - 21 Nash Drive\t5
+PR0070\tPR0070 - 3 Sperling Road\t4
+PR0073E\tPR0073 - Primrose Lodge - East\t10
+PR0073W\tPR0073 - Primrose Lodge - West\t10
+PR0075H\tPR0075  Aster Lodge,119-121 Wendover Road, Aylesbury, Bucks, HP21 9LW, Started 02/03/2015\t6
+PR0075M\tPR0075  Aster Lodge,119-121 Wendover Road, Aylesbury, Bucks, HP21 9LW, Started 02/03/2015\t5
+PR0075L\tPR0075  Aster Lodge,119-121 Wendover Road, Aylesbury, Bucks, HP21 9LW, Started 02/03/2015\t1
+PR0078\tPR0078 - 3 Church Close, Hayes, Middlesex, UB4 8JW\t6
+PR0079\tPR0079 - 32 Dolphin Road, Slough, SL1 1TD\t8
+PR0081\tPR0081 - 251A Park Road, North Uxbridge, Hillingdon, UB8 1NS\t7
+PR0084\tPR0084 - 105 Sweet Croft Lane, Uxbridge, Middlesex, UB10 9LG\t7
+PR0085\tPR0085 - 75 Green Lane, New Malden, Surrey, KT3 5BX\t5
+PR086A\tPR0086 - Hawthorns, Bath Road, Maidenhead, SL6 0AP\t7
+PR086B\tPR0086 - Hawthorns, Bath Road, Maidenhead, SL6 0AP\t4
+PR0087\tPR0087 - 2C Cromwell Road, Camberley, Surrey, GU15 4HY\t3
+PR0091\tPR0091 - 123 Wills Crescent, Whitton, Hounslow, TW3 2JE\t6
+PR0092\tPR0092 - 128 Coventon Road, Aylesbury, Bucks, HP19 9LL\t5
+PR0093\tPR0093 - 17 Campion Close, Uxbridge, UB8 3PY\t7
+PR0094\tPR0094 - 84 Weston Drive, Stanmore, Harrow, HA7 2EN\t6
+PR0095\tPR0095 - 55 Vista Way, Harrow, HA3 0SP\t6
+PR0098\tPR0098 - 2 Riverside Close Wallington, Surrey, SM6 7DH\t6
+PR0099\tPR0099 - 71 Stanley Park Road, Carshalton, Surrey, SM5 3HX\t7
+PR0100\tPR0100 - 50 Rectory Grove, Hampton, Middlesex, TW12 1AH\t6
+PR0101\tPR0101 - 21A Cleveland Road , Uxbridge UB8 2DR\t7
+PR0104\tPR0104 - 37 Lucien Road, Tooting\t6
+PR0105\tPR0105 - 165 Frenches Road, Redhill RH1 2HZ\t6
+PR0106\tPR0106 - Silver Birches, Court Drive, Hillingdon UB10 0BW\t6
+PR0107\tPR0107 - 76 Thornhill Road, Ickenham, Uxbridge UB10 8SH\t5
+PR0108\tPR108 - 167/167A Frenches Road, Redhill, RH1 2HZ\t6
+PR0114\tPR0114 - Lanterns Bungalow, 42 West Road, Guildford, GU1 2AT\t2
+PR0115\tPR0115 - 238 Lynmouth Avenue, Morden, SM4 4RS\t6
+PR0117\tPR0117 - 63 Talbot Road, Harrow, Middlesex , HA3 7QE\t6
+PR0121\tPR0121 - 118 The Frithe, Wexham, Slough, Berkhsire, SL2 5RP\t6
+PR0021\tPR0021 - Flat 1, 237 Haydon Road , Wimbledon , London , SW19 8TY\t1
+PR0126\tPR00126 - Flat 2, 237 Haydons Road, Wimbledon, London, SW19 8TY\t1
+PR0127\tPR0127 - Flat 3, 237 Haydons Road, Wimbledon, London, SW19 8TY\t1
+PR0128\tPR0128 - Rose Cottage, 1C Dryden Road, London , SW19 8SQ\t2
+PR0133\tPR0133 - Flat 1, 68 Alpha Street South, Slough, SL1 1QX\t1
+PR0134\tPR0134 - Flat 2, 68 Alpha Street South, Slough, SL1 1QX\t1
+PR0135\tPR0135 - Flat 3, 68 Alpha Street South, Slough, SL1 1QX\t1
+PR0136\tPR0136 - Flat 4, 68 Alpha Street South, Slough, SL1 1QX\t1
+PR0137\tPR0137 - Flat 5, 68 Alpha Street South, Slough, SL1 1QX\t1
+PR0138\tPR0138 - Flat 6, 68 Alpha Street South, Slough, SL1 1QX\t1
+PR0139\tPR0139 - Flat 7, 68 Alpha Street South, Slough, SL1 1QX\t1
+PR0140\tPR0140 - Flat 8, 68 Alpha Street South, Slough, SL1 1QX\t1
+PR0145A\tPR0145A - Apple Blossom Lodge, Ground Floor, 64-66 Ickenham Road, Ruislip, HA4 7DQ\t6
+PR0147\tPR0147 - 37 Station Road, Uxbridge, UB8 3AB\t6
+PR0152\tPR0152 - 149 Thetford Road, New Malden, KT3 5DZ\t7
+PR0149\tPR0149 - 1 Dorset Way,Uxbridge\t6
+PR0153A\tPR0153A - Maple Tree Lodge,Ground Flour, Limecroft Road, Knaphill, Surrey GU21 2TH\t7
+PF0043\tFlat 26 Glenister Gardens, Hayes, UB3 3FA \t1
+PF0044\tFlat 27 Glenister Gardens, Hayes, UB3 3FA \t1
+PF0045\tFlat 28 Glenister Gardens, Hayes, UB3 3FA \t1
+PF0046\tFlat 29 Glenister Gardens, Hayes, UB3 3FA \t1
+PF0047\tFlat 30 Glenister Gardens, Hayes, UB3 3FA \t1
+PF0049\tFlat 32 Glenister Gardens, Hayes, UB3 3FA \t1
+PF0050\tFlat 33 Glenister Gardens, Hayes, UB3 3FA \t1
+PF0051\tFlat 34 Glenister Gardens, Hayes, UB3 3FA \t1
+PF0052\tFlat 35 Glenister Gardens, Hayes, UB3 3FA \t1
+PF0053\tFlat 36 Glenister Gardens, Hayes, UB3 3FA \t1
+PF0054\tFlat 37 Glenister Gardens, Hayes, UB3 3FA \t1
+PF0055\tFlat 38 Glenister Gardens, Hayes, UB3 3FA \t1
+PF0056\tFlat 1, Honeycroft Hill House, 60 Honeycroft Hill, Uxbridge, UB10 9NS\t1
+PF0057\tFlat 2, Honeycroft Hill House, 60 Honeycroft Hill, Uxbridge, UB10 9NS\t1
+PF0058\tFlat 3, Honeycroft Hill House, 60 Honeycroft Hill, Uxbridge, UB10 9NS\t1
+PF0059\tFlat 4, Honeycroft Hill House, 60 Honeycroft Hill, Uxbridge, UB10 9NS\t1
+PF0060\tFlat 5, Honeycroft Hill House, 60 Honeycroft Hill, Uxbridge, UB10 9NS\t1
+PF0061\tFlat 6, Honeycroft Hill House, 60 Honeycroft Hill, Uxbridge, UB10 9NS\t1
+PF0062\tFlat 7, Honeycroft Hill House, 60 Honeycroft Hill, Uxbridge, UB10 9NS\t1
+PF0063\tFlat 8, Honeycroft Hill House, 60 Honeycroft Hill, Uxbridge, UB10 9NS\t1
+PF0064\tFlat 9, Honeycroft Hill House, 60 Honeycroft Hill, Uxbridge, UB10 9NS\t1
+PF0065\tFlat 10, Honeycroft Hill House, 60 Honeycroft Hill, Uxbridge, UB10 9NS\t1
+PF0066\tFlat 11, Honeycroft Hill House, 60 Honeycroft Hill, Uxbridge, UB10 9NS\t1
+PF0067\tFlat 12, Honeycroft Hill House, 60 Honeycroft Hill, Uxbridge, UB10 9NS\t1
+PF0068\tFlat 13, Honeycroft Hill House, 60 Honeycroft Hill, Uxbridge, UB10 9NS\t1
+PF0069\tFlat 14, Honeycroft Hill House, 60 Honeycroft Hill, Uxbridge, UB10 9NS\t1
+PF0070\tFlat 15, Honeycroft Hill House, 60 Honeycroft Hill, Uxbridge, UB10 9NS\t1
+PF0071\tFlat 16, Honeycroft Hill House, 60 Honeycroft Hill, Uxbridge, UB10 9NS\t1
+PF0073\tFlat 1, Church Road, Cowley, Uxbridge, Middlesex, UB8 3NA\t1
+PF0074\tFlat 2, Church Road, Cowley, Uxbridge, Middlesex, UB8 3NA\t1
+PF0075\tFlat 3, Church Road, Cowley, Uxbridge, Middlesex, UB8 3NA\t1
+PF0076\tFlat 4, Church Road, Cowley, Uxbridge, Middlesex, UB8 3NA\t1
+PF0077\tFlat 5, Church Road, Cowley, Uxbridge, Middlesex, UB8 3NA\t1
+PF0078\tFlat 6, Church Road, Cowley, Uxbridge, Middlesex, UB8 3NA\t1
+PF0081\tFlat 2, Low Need, Swan Road, West Drayton, London, UB7 7LA\t1
+PF0082\tFlat 3, High Need, Swan Road, West Drayton, London, UB7 7LA\t1
+PF0083\tFlat 4, High Need, Swan Road, West Drayton, London, UB7 7LA\t1
+PF0084\tFlat 5, High Need, Swan Road, West Drayton, London, UB7 7LA\t1
+PF0085\tFlat 6, High Need, Swan Road, West Drayton, London, UB7 7LA\t1
+PF0086\tFlat 7, High Need, Swan Road, West Drayton, London, UB7 7LA\t1
+PF0087\tFlat 8, High Need, Swan Road, West Drayton, London, UB7 7LA\t1
+PF0088\tFlat 9, High Need, Swan Road, West Drayton, London, UB7 7LA\t1
+PF0089\tFlat 10, High Need, Swan Road, West Drayton, London, UB7 7LA\t1
+PF0090\tFlat 11, Low Need, Swan Road, West Drayton, London, UB7 7LA\t1
+PF0091\tFlat 12, Low Need, Swan Road, West Drayton, London, UB7 7LA\t1
+PF0092\tFlat 13, Low Need, Swan Road, West Drayton, London, UB7 7LA\t1
 PF0093\tFlat 14, Low Need, Swan Road, West Drayton, London, UB7 7LA\t1
-PF0094\tFlat 15, Low Need, Swan Road, West Drayton, London, UB7 7LA\t1
+PF0094\tFlat 15,Low Need, Swan Road, West Drayton, London, UB7 7LA\t1
 PF0095\tFlat 16, Low Need, Swan Road, West Drayton, London, UB7 7LA\t1
 PF0096\tFlat 17, Low Need, Swan Road, West Drayton, London, UB7 7LA\t1
 PF0097\tFlat 18, Low Need, Swan Road, West Drayton, London, UB7 7LA\t1
@@ -680,7 +686,7 @@ if st.session_state.get("processed", False):
                 )
             else:
                 filtered_dashboard_df = property_detail_df
-                fixed_total_beds = int(property_beds_df["TOTAL BEDS"].sum())
+                fixed_total_beds = 539  # Exact total beds sum
                 total_occupied_eops = property_detail_df["SUID"].nunique()
                 total_vacant_beds = max(
                     0, fixed_total_beds - total_occupied_eops
